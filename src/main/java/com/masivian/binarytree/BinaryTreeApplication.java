49d33860eafe5e22273d6c5f86229f79394a0ccd
dfsanchezg <dfsg@outlook.com>
2019-10-14 18:17:07 -0500

    refactor the main class name

--- src/main/java/com/masivian/binarytree/BinaryTreeApplication.java
+++ src/main/java/com/masivian/binarytree/BinaryTreeApplication.java
@@ -3,11 +3,14 @@ package com.masivian.binarytree;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The spring boot main application
+ */
 @SpringBootApplication
-public class BinarytreeApplication {
+public class BinaryTreeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BinarytreeApplication.class, args);
+		SpringApplication.run(BinaryTreeApplication.class, args);
 	}
 
 }
